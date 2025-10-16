0cadf27fe2759f0380d147daeb412490a5ed43ac
Irvin Cote <irvincoteg@gmail.com>
2025-10-16 04:31:14 +0200

    DB : Adding a docker compose yaml with a postgres and a migration service which triggers a setup sql file

---
+++ clients-contracts-db/migrations/00-setup.sql
@@ -0,0 +1,27 @@
+-- clients-contracts-db/00-setup.sql
+-- Master bootstrap script intended for docker-entrypoint-initdb.d usage.
+-- Executes base schema migrations located under /sql inside the container.
+
+\echo 'Applying base schema...'
+\i ./base/01-schema.sql
+
+\echo 'Applying constraints...'
+\i ./base/02-constraints.sql
+
+\echo 'Applying triggers...'
+\i ./base/03-triggers.sql
+
+\echo 'Applying views...'
+\i ./base/04-views.sql
+
+\echo 'Applying performance indexes...'
+\i ./base/05-indexes.sql
+
+\echo 'Loading client fixtures...'
+\i ./fixtures/10-fixtures-clients.sql
+
+\echo 'Loading contract fixtures...'
+\i ./fixtures/11-fixtures-contracts.sql
+
+\echo 'Database bootstrap completed.'
+
